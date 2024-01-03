2f03fb860ae368f10f1b9b9595310193ac7d69d5
bohdancho <bogdan.chornocondratenko@gmail.com>
2024-01-03 13:59:24 +0100

    fix vscubing macro

--- .local/bin/tmux-vscubing.sh
+++ .local/bin/tmux-vscubing.sh
@@ -11,24 +11,23 @@ if [ "$SESSIONEXISTS" = "" ]; then
 
     # Window 1: vscubing-frontend
     tmux new-window -t $SESSION:1 -n 'vscubing-frontend'
-    tmux send-keys -t 'vscubing-frontend' 'cd ~/projects/vscubing/vscubing-frontend' C-m 'vim' C-m
+    tmux send-keys -t $SESSION:1 'cd ~/projects/vscubing/vscubing-frontend' C-m 'vim' C-m
 
     # Window 2: vscubing-frontend - Run dev server
     tmux new-window -t $SESSION:2 -n 'vscubing-frontend-dev'
-    tmux send-keys -t 'vscubing-frontend-dev' 'cd ~/projects/vscubing/vscubing-frontend' C-m 'bun run dev' C-m
+    tmux send-keys -t $SESSION:2 'cd ~/projects/vscubing/vscubing-frontend' C-m 'bun run dev --open' C-m
 
     # Window 3: vscubing-backend
     tmux new-window -t $SESSION:3 -n 'vscubing-backend'
-    tmux send-keys -t 'vscubing-backend' 'cd ~/projects/vscubing/vscubing-backend' C-m 'source venv/bin/activate' C-m 'python3 manage.py runserver' C-m
+    tmux send-keys -t $SESSION:3 'cd ~/projects/vscubing/vscubing-backend' C-m 'source venv/bin/activate' C-m 'python3 manage.py runserver' C-m
 
     # Window 4: alg.cubing.net
     tmux new-window -t $SESSION:4 -n 'alg.cubing.net'
-    tmux send-keys -t 'alg.cubing.net' 'cd ~/projects/vscubing/alg.cubing.net' C-m 'live-server src/alg.cubing.net --no-browser' C-m
+    tmux send-keys -t $SESSION:4 'cd ~/projects/vscubing/alg.cubing.net' C-m 'live-server src/alg.cubing.net --no-browser' C-m
 
     # Window 5: cstimer
     tmux new-window -t $SESSION:5 -n 'cstimer'
-    tmux send-keys -t 'cstimer' 'cd ~/projects/vscubing/cstimer' C-m 'make' C-m 'php -S 127.0.0.1:8081 -t dist' C-m
+    tmux send-keys -t $SESSION:5 'cd ~/projects/vscubing/cstimer' C-m 'make' C-m 'php -S 127.0.0.1:8081 -t dist' C-m
 fi
 
-# Attach Session, on the first window
-tmux switch-client -t $SESSION:1
+tmux attach-session -t $SESSION:1
