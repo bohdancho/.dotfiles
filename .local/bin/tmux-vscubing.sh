bab728b6436b9348ad83a2f426fd5796d08d03a0
bohdancho <bogdan.chornocondratenko@gmail.com>
2024-02-10 11:50:31 +0100

    tmux-vscubing remove alg.cubing.net

--- .local/bin/tmux-vscubing.sh
+++ .local/bin/tmux-vscubing.sh
@@ -7,7 +7,6 @@ SESSIONEXISTS=$(tmux list-sessions | grep $SESSION)
 # Only create tmux session if it doesn't already exist
 if [ "$SESSIONEXISTS" = "" ]; then
     # Start New Session with our name
-    tmux new-session -d -s $SESSION
     tmux new-session -d -s $SESSION -c "$HOME/projects/vscubing"
 
     # Window 1: vscubing-frontend
@@ -19,16 +18,12 @@ if [ "$SESSIONEXISTS" = "" ]; then
     tmux send-keys -t $SESSION:2 'cd ~/projects/vscubing/vscubing-frontend' C-m 'bun run dev --open' C-m
 
     # Window 3: vscubing-backend
-    tmux new-window -t $SESSION:3 -n 'vscubing-server'
+    tmux new-window -t $SESSION:3 -n 'backend-server'
     tmux send-keys -t $SESSION:3 'cd ~/projects/vscubing/vscubing-backend' C-m 'source venv/bin/activate' C-m 'python3 manage.py runserver' C-m
 
-    # Window 4: alg.cubing.net
-    tmux new-window -t $SESSION:4 -n 'alg.cubing.net'
-    tmux send-keys -t $SESSION:4 'cd ~/projects/vscubing/alg.cubing.net' C-m 'live-server src/alg.cubing.net --no-browser' C-m
-
-    # Window 5: cstimer
-    tmux new-window -t $SESSION:5 -n 'cstimer'
-    tmux send-keys -t $SESSION:5 'cd ~/projects/vscubing/cstimer' C-m 'make' C-m 'php -S 127.0.0.1:8081 -t dist' C-m
+    # Window 4: cstimer
+    tmux new-window -t $SESSION:4 -n 'cstimer'
+    tmux send-keys -t $SESSION:4 'cd ~/projects/vscubing/cstimer' C-m 'make' C-m 'php -S 127.0.0.1:8081 -t dist' C-m
 fi
 
 tmux switch-client -t $SESSION:1
